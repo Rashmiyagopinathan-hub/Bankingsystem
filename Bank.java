6aee9eb00855a431daf3b90ff95662bcf6792713
Rogasree <rogasri79@gmail.com>
2026-08-18 21:39:31 +0530

    Add main

--- Bank.java
+++ Bank.java
@@ -3,16 +3,14 @@ import java.util.HashMap;
 public class Bank {
     HashMap<Integer, Account> map = new HashMap<>();
 
-    void create(int accNo, String name, int bal) {
-        map.put(accNo, new Account(name, bal));
+    void create(int no, String name, int bal) {
+        map.put(no, new Account(name, bal));
     }
 
-    void withdraw(int accNo, int amt) {
-        Account a = map.get(accNo);
-        if (a != null && a.balance >= amt) {
-            a.balance -= amt;
-            System.out.println("Withdraw: " + amt);
-            System.out.println("Balance: " + a.balance);
-        } else System.out.println("Insufficient Balance");
+    void display(int no) {
+        Account a = map.get(no);
+        System.out.println("Account No: " + no);
+        System.out.println("Name: " + a.name);
+        System.out.println("Balance: " + a.balance);
     }
 }
--- Main.java
+++ Main.java
@@ -1,16 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
         Bank b = new Bank();
 
         b.create(101, "Rashmi", 5000);
 
-        System.out.println("=== BANKING SYSTEM ===");
-        System.out.println("Account Created");
-        System.out.println("Name : Rashmi");
-        System.out.println("Balance : 5000");
+        while (true) {
+            System.out.println("\n=== BANKING SYSTEM ===");
+            System.out.println("1. Display");
+            System.out.println("2. Exit");
+            System.out.print("Enter choice: ");
 
-        System.out.println("\nWithdraw 2000");
-        b.withdraw(101, 2000);
+            int choice = input.nextInt();
+
+            if (choice == 1) {
+                b.display(101);
+            } else if (choice == 2) {
+                System.out.println("Thank you!");
+                break;
+            } else {
+                System.out.println("Invalid choice");
+            }
+        }
+
+        input.close();
     }
 }
